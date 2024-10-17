7714300a812327d730d064aa7129dd83cd9af86c
Ahmed Hussein <wolktheline@yahoo.com>
2024-10-17 09:39:50 -0600

    Class variables

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 class Item:
+    pay_rate = 0.8  # Applying 20% discount
+
     def __init__(self, name: str, price: float, quantity=0):
         # Validate the data type constructor received
-        assert price >= 0
-        assert quantity >= 0
+        assert price >= 0, f"Price {price} is not greater than zero"
+        assert quantity >= 0, f"Quantity {quantity}, is of wrong value"
 
         # Assign to self object
         self.name = name
@@ -20,3 +22,5 @@ print(item1.calculate_total())
 item2 = Item("Laptop", 1000, 5)
 
 item2.has_numpad = False
+
+print(Item.pay_rate)
